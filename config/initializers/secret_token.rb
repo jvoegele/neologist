77c4b64ddae1f7650e0e284a2c63f79f9e84aa19
Jason Voegele <jason@jvoegele.com>
2013-07-31 20:45:24 -0400

    Initial import of Neologist application skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neologist::Application.config.secret_token = '0af2c9b50b5b8dcde0b8fabbe4bff440318e5d0ec876bfb0df99a530560dafea5340fae3527cd01318bc996e3d1572b8d788915cb7b6023a29ca846d8edfc6a0'
